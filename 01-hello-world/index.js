d760c602fd6b8f8dd68c1c4cf02703dd38864b4b
Shandy Nguyen <63096378+shandyprofile@users.noreply.github.com>
2025-01-18 08:43:59 +0700

    update project hello

--- 01-hello-world/index.js
+++ 01-hello-world/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const server = 'localhost'
 
 app.get('/', (req, res) => {
   res.send('Hello World! My name is Hieu Nguyen')
 })
 
-app.listen(port, () => {
+app.listen(port, server, () => {
   console.log(`Example app listening on port ${port}`)
 })
